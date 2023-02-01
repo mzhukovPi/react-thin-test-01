b6af4613b6e8796bbaf34c268ffc36516bb1caca
mzhukovPi <mzhukov31415dev2@gmail.com>
2023-02-01 13:28:08 +0300

    Another commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+        <img src={logo} className='App-logo' alt='logo'/>
         <p>(Ed)it <code>src/App.js</code> and save to reload.</p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>Learn React</a>
         <p>BACKEND_URL: {process.env.BACKEND_URL}</p>
--- src/vitals.js
+++ src/vitals.js
@@ -1,17 +1,17 @@
-const vitalsUrl = 'https://vitals.vercel-analytics.com/v1/vitals';
+const vitalsUrl = 'https://vitals.vercel-analytics.com/v1/vitals'
 
 function getConnectionSpeed() {
   return 'connection' in navigator &&
     navigator['connection'] &&
     'effectiveType' in navigator['connection']
     ? navigator['connection']['effectiveType']
-    : '';
+    : ''
 }
 
 export function sendToVercelAnalytics(metric) {
-  const analyticsId = process.env.REACT_APP_VERCEL_ANALYTICS_ID;
+  const analyticsId = process.env.REACT_APP_VERCEL_ANALYTICS_ID
   if (!analyticsId) {
-    return;
+    return
   }
 
   const body = {
@@ -21,20 +21,20 @@ export function sendToVercelAnalytics(metric) {
     href: window.location.href,
     event_name: metric.name,
     value: metric.value.toString(),
-    speed: getConnectionSpeed(),
-  };
+    speed: getConnectionSpeed()
+  }
 
   const blob = new Blob([new URLSearchParams(body).toString()], {
     // This content type is necessary for `sendBeacon`
-    type: 'application/x-www-form-urlencoded',
-  });
+    type: 'application/x-www-form-urlencoded'
+  })
   if (navigator.sendBeacon) {
-    navigator.sendBeacon(vitalsUrl, blob);
+    navigator.sendBeacon(vitalsUrl, blob)
   } else
     fetch(vitalsUrl, {
       body: blob,
       method: 'POST',
       credentials: 'omit',
-      keepalive: true,
-    });
+      keepalive: true
+    })
 }
